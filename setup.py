cdc593bf85c51749913b18c3a084822343f1961b
AmirAhmed0s <163478540+AmirAhmed0s@users.noreply.github.com>
2024-11-17 23:01:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
 	install_requires = f.read().strip().split('\n')
 
-# get version from __version__ variable in whitelabel/__init__.py
-from whitelabel import __version__ as version
+# get version from __version__ variable in ksa_zatca/__init__.py
+from ksa_zatca import __version__ as version
 
 setup(
-	name='whitelabel',
+	name='ksa_zatca',
 	version=version,
-	description='ERPNext Whitelabel',
+	description='ERPNext ksa_zatca',
 	author='Bhavesh Maheshwari',
 	author_email='maheshwaribhavesh95863@gmail.com',
 	packages=find_packages(),
