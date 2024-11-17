7527caf85959f2a2fca4efce3646cd932a593151
AmirAhmed0s <163478540+AmirAhmed0s@users.noreply.github.com>
2024-11-17 23:04:22 +0200

    Update whitelabel_setting.js

--- whitelabel/whitelabel/doctype/whitelabel_setting/whitelabel_setting.js
+++ whitelabel/whitelabel/doctype/whitelabel_setting/whitelabel_setting.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2021, Bhavesh Maheshwari and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Whitelabel Setting', {
+frappe.ui.form.on('ksa_zatca Setting', {
 	after_save: function(frm) {
 		frappe.ui.toolbar.clear_cache();
 	}
