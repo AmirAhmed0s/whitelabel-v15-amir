76884d1af6826abab49c66e71e8ccf416a263306
AmirAhmed0s <163478540+AmirAhmed0s@users.noreply.github.com>
2024-11-17 23:04:53 +0200

    Update whitelabel.js

--- whitelabel/public/js/whitelabel.js
+++ whitelabel/public/js/whitelabel.js
@@ -1,20 +1,20 @@
 $(window).on('load', function() {
     frappe.after_ajax(function () {
-        if (frappe.boot.whitelabel_setting.show_help_menu) {
+        if (frappe.boot.ksa_zatca_setting.show_help_menu) {
             // $('.dropdown-help').css('display','block');
             $('.dropdown-help').attr('style', 'display: block !important');
         }
-        if (frappe.boot.whitelabel_setting.logo_width) {
-            $('.app-logo').css('width',frappe.boot.whitelabel_setting.logo_width+'px');
+        if (frappe.boot.ksa_zatca_setting.logo_width) {
+            $('.app-logo').css('width',frappe.boot.ksa_zatca_setting.logo_width+'px');
         }
-        if (frappe.boot.whitelabel_setting.logo_height) {
-            $('.app-logo').css('height',frappe.boot.whitelabel_setting.logo_height+'px');
+        if (frappe.boot.ksa_zatca_setting.logo_height) {
+            $('.app-logo').css('height',frappe.boot.ksa_zatca_setting.logo_height+'px');
         }
-        if (frappe.boot.whitelabel_setting.navbar_background_color) {
-            $('.navbar').css('background-color',frappe.boot.whitelabel_setting.navbar_background_color)
+        if (frappe.boot.ksa_zatca_setting.navbar_background_color) {
+            $('.navbar').css('background-color',frappe.boot.ksa_zatca_setting.navbar_background_color)
         }
-        if (frappe.boot.whitelabel_setting.custom_navbar_title_style && frappe.boot.whitelabel_setting.custom_navbar_title) {
-            $(`<span style=${frappe.boot.whitelabel_setting.custom_navbar_title_style.replace('\n','')} class="hidden-xs hidden-sm">${frappe.boot.whitelabel_setting.custom_navbar_title}</span>`).insertAfter("#navbar-breadcrumbs")
+        if (frappe.boot.ksa_zatca_setting.custom_navbar_title_style && frappe.boot.ksa_zatca_setting.custom_navbar_title) {
+            $(`<span style=${frappe.boot.ksa_zatca_setting.custom_navbar_title_style.replace('\n','')} class="hidden-xs hidden-sm">${frappe.boot.ksa_zatca_setting.custom_navbar_title}</span>`).insertAfter("#navbar-breadcrumbs")
         }
     })
 })
