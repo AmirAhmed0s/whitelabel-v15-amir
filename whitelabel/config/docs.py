6afe23bc08d5cbc14fdbe93ecfb757aea4471d63
AmirAhmed0s <163478540+AmirAhmed0s@users.noreply.github.com>
2024-11-17 23:05:39 +0200

    Update docs.py

--- whitelabel/config/docs.py
+++ whitelabel/config/docs.py
@@ -2,10 +2,10 @@
 Configuration for docs
 """
 
-# source_link = "https://github.com/[org_name]/whitelabel"
-# docs_base_url = "https://[org_name].github.io/whitelabel"
+# source_link = "https://github.com/[org_name]/ksa_zatca"
+# docs_base_url = "https://[org_name].github.io/ksa_zatca"
 # headline = "App that does everything"
 # sub_heading = "Yes, you got that right the first time, everything"
 
 def get_context(context):
-	context.brand_html = "Whitelabel"
+	context.brand_html = "ksa_zatca"
